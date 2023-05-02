eae2bac9c4f82d8b3ef4dd0015ad51357f0a62e8
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-02 13:42:20 -0500

    completed WHERE exercises

---
+++ where_exercises.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+SELECT first_name, last_name, gender FROM employees
+                             WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+                                AND gender = 'M';
+
+SELECT first_name, last_name FROM employees WHERE first_name LIKE 'E%';
+
+SELECT first_name, last_name FROM employees
+                             WHERE last_name LIKE '%Q%'
+                                AND last_name NOT LIKE '%QU%';
+
+SELECT first_name, last_name FROM employees
+                             WHERE last_name LIKE 'E%'
+                                OR last_name LIKE '%E';
