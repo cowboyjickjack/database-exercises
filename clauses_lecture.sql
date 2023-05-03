abecc89d2cfcab1d889dd4252c2a5f6646e67976
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-03 16:07:02 -0500

    completed group by exercises

--- clauses_lecture.sql
+++ clauses_lecture.sql
@@ -26,3 +26,27 @@ SELECT DATEDIFF(NOW(), hire_date)/365 FROM employees LIMIT 50;
 
 SELECT MINUTE(NOW());
 SELECT SECOND(NOW());
+
+# Group By and Aggregate Functions
+
+# Group By will combine duplicates into one value for each group
+# It will consolidate rows based on a common column (similar to DISTINCT)
+# Must use with something you're selecting. If it's not part of the Group By and non-aggregated, it will show an error
+
+SELECT last_name FROM employees GROUP BY last_name;
+
+# shows how many values are in the first name column (how many first names there are)
+SELECT COUNT(first_name) FROM employees;
+
+# gives a count of how many ppl have each last name
+SELECT last_name, COUNT(first_name) FROM employees GROUP BY last_name ORDER BY last_name;
+
+# shows how many males vs females
+SELECT gender, COUNT(first_name) FROM employees GROUP BY gender;
+
+# how old each employee was when hired, but MIN shows youngest person ever hired
+SELECT MIN(DATEDIFF(hire_date, birth_date)) / 365 FROM employees;
+# oldest person ever hired
+SELECT MAX(DATEDIFF(hire_date, birth_date)) / 365 FROM employees;
+
+SELECT AVG(DATEDIFF(hire_date, birth_date)) / 365 FROM employees;
---
+++ group_by_exercises.sql
@@ -0,0 +1,20 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT COUNT(first_name, last_name) FROM employees WHERE
+                                                last_name LIKE '%E' AND
+                                                last_name LIKE 'E%' GROUP BY first_name, last_name
+                                                                    ORDER BY last_name;
+
+SELECT COUNT(last_name), last_name FROM employees WHERE
+                                    last_name LIKE '%q%' AND
+                                    last_name NOT LIKE '%qu%' GROUP BY last_name
+                                                              ORDER BY last_name;
+
+# Update your query for 'Irena', 'Vidya', or 'Maya'.
+# Use count(*) and GROUP BY to find the number of employees for each gender with those names.
+SELECT COUNT(first_name), gender FROM employees WHERE
+                                                       first_name = 'Irena' OR
+                                                       first_name = 'Vidya' OR
+                                                       first_name = 'Maya' GROUP BY gender;
