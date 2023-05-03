4ffe623720461adcc4fd43dc5484a2c4a3a24584
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-03 16:27:20 -0500

    completed aliases exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,7 +12,9 @@ SELECT COUNT(last_name), last_name FROM employees WHERE
                                     last_name NOT LIKE '%qu%' GROUP BY last_name
                                                               ORDER BY last_name;
 
-SELECT COUNT(first_name), gender FROM employees WHERE
+# AS = Aliases
+# Changes the header of the row to clean it up
+SELECT COUNT(first_name) AS Quantity, gender FROM employees WHERE
                                                        first_name = 'Irena' OR
                                                        first_name = 'Vidya' OR
                                                        first_name = 'Maya' GROUP BY gender;
