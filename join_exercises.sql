1055f206279b46d1044856b2bae53f8a78d09a55
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-05 09:27:14 -0500

    completing join walkthrough

--- join_exercises.sql
+++ join_exercises.sql
@@ -47,3 +47,30 @@ FROM users
      RIGHT JOIN roles ON users.role_id = roles.id;
 
 # EXERCISE TWO
+
+USE employees;
+
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
+    FROM departments d
+        JOIN dept_manager dm on d.dept_no = dm.dept_no
+        JOIN employees e on e.emp_no = dm.emp_no
+        WHERE to_date LIKE '9%'
+        ORDER BY dept_name;
+
+# Departments currently managed by women
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
+FROM departments d
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e on e.emp_no = dm.emp_no
+            WHERE to_date LIKE '9%'
+            AND gender = 'F'
+            ORDER BY dept_name;
+
+# Find the current titles of employees currently working in the customer service dept
+SELECT title, COUNT(title) AS Total FROM titles t
+    JOIN dept_emp de on t.emp_no = de.emp_no
+    JOIN departments d on d.dept_no = de.dept_no
+    WHERE dept_name = 'Customer Service'
+    AND t.to_date LIKE '9%'
+    AND de.to_date LIKE '9%'
+    GROUP BY title;
