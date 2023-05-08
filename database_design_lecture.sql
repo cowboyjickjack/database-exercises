3ce1685b0b2369b6e7d980d1fe83d78488ae8280
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-08 09:14:32 -0500

    updating last changes

--- database_design_lecture.sql
+++ database_design_lecture.sql
@@ -33,3 +33,62 @@ INSERT INTO authors (name) VALUES
     ('Biggie Smalls'),
     ('Malcolm X');
 
+# Adding column, then the foreign key after
+ALTER TABLE quotes
+    ADD COLUMN author_id INT NOT NULL;
+
+UPDATE quotes
+SET author_id = (
+    SELECT id
+    FROM authors
+    WHERE authors.name = quotes.author);
+
+ALTER TABLE quotes DROP COLUMN author;
+
+# NOW we can add our constraint
+ALTER TABLE quotes
+    ADD CONSTRAINT quotes_author_id_fk
+        FOREIGN KEY (author_id)
+            REFERENCES authors (id);
+
+CREATE TABLE topics (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    topic VARCHAR(60) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO topics (topic) VALUES
+    ('Life and death'),
+    ('The world as a stage'),
+    ('Good and evil'),
+    ('Contentment and materialism'),
+    ('Sleep and death'),
+    ('Courage and bravery'),
+    ('Personal responsibility'),
+    ('Love and perception'),
+    ('Standing up for oneself'),
+    ('Moving on and accepting the past'),
+    ('Solitude and isolation');
+
+CREATE TABLE quotes_topics (
+       quote_id INT NOT NULL,
+       topic_id INT NOT NULL,
+       CONSTRAINT quotes_topics_quote_id_fk FOREIGN KEY (quote_id) REFERENCES quotes(id),
+       CONSTRAINT quotes_topics_topic_id_fk FOREIGN KEY (topic_id) references topics(id)
+);
+
+INSERT INTO quotes_topics (quote_id, topic_id)
+VALUES
+    (1, 1), (1, 3), (1, 9),
+    (2, 2), (2, 7), (2, 11),
+    (3, 2), (3, 3), (3, 5),
+    (4, 1), (4, 4), (4, 8),
+    (5, 1), (5, 5), (5, 6),
+    (6, 3), (6, 5), (6, 6),
+    (7, 3), (7, 6), (7, 9),
+    (8, 1), (8, 4), (8, 8),
+    (9, 3), (9, 6), (9, 9),
+    (10, 2), (10, 4), (10, 10),
+    (11, 5), (11, 8), (11, 11);
+
+SELECT content, to
